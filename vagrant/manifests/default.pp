defb440cc5c24709654a0722582b1c96d599beca
Robin van der Vleuten <robinvdvleuten@gmail.com>
2013-04-19 12:48:28 +0200

    Added support for installing pear/pecl extensions.

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -3,4 +3,11 @@ node default {
   include git
 
   phpenv::php { "5.4.14": }
+
+  phpenv::extension { "PHPUnit":
+    extension => "PHPUnit",
+    channel   => "pear.phpunit.de",
+    # todo come with a better option for this.
+    require   => Exec["set 5.4.14 as global"]
+  }
 }
